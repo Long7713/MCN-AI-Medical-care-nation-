5ae1c240474371c34800b3f78544d6cd078e05d4
Long <vtran3331@gmail.com>
2026-06-16 21:11:16 +0700

    Fix for more clean

--- Systemhospital/download-AI.py
+++ Systemhospital/download-AI.py
@@ -9,7 +9,7 @@ tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base-v2')
 print("--- HỆ THỐNG: Đang xuất mô hình về thư mục dự án (ổ D)... ---")
 
 # 2. Lưu một bản sao "sạch" trực tiếp vào thư mục dự án với tên là 'phobert_v2'
-model.save_pretrained('./phobert_v2')
-tokenizer.save_pretrained('./phobert_v2')
+model.save_pretrained('../phobert_v2')
+tokenizer.save_pretrained('../phobert_v2')
 
 print("--- HỆ THỐNG: ĐÃ ĐƯA AI VỀ THƯ MỤC DỰ ÁN THÀNH CÔNG! ---")/
